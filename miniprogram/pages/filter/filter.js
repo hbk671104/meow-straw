c1d7f7c7f6ac889d4343fe20a0705a7400ee935a
hbk671104 <hbk671104@gmail.com>
2020-02-27 19:08:34 +0800

    add local data

--- miniprogram/pages/filter/filter.js
+++ miniprogram/pages/filter/filter.js
@@ -1,5 +1,5 @@
 // miniprogram/pages/filter/filter.js
-import { fetchBreed } from '../../service/api.js'
+import { breeds } from '../../data/breeds.js'
 
 Page({
 
@@ -7,23 +7,13 @@ Page({
    * 页面的初始数据
    */
   data: {
-    breeds: []
+    breeds
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    wx.showLoading({
-      title: '加载中...',
-    })
-    this.loadBreed(() => wx.hideLoading())
   },
-  loadBreed: function(callback) {
-    fetchBreed().then(data => {
-      this.setData({
-        breeds: data
-      })
-    }).finally(callback)
-  }
+
 })
