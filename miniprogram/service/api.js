373fe9b5e44d218da16c488ae182e989aa28a800
hbk671104 <hbk671104@gmail.com>
2020-02-28 12:11:35 +0800

    api param fix

--- miniprogram/service/api.js
+++ miniprogram/service/api.js
@@ -3,14 +3,8 @@ import { fetch } from '../utils/util.js'
 
 const base_url = 'https://api.thecatapi.com/v1'
 
-export const search = (params = {}) => fetch({
-  url: `${base_url}/images/search?${queryString.stringify(params)}`
-})
+export const search = (params = {}) => fetch(`${base_url}/images/search?${queryString.stringify(params)}`)
 
-export const fetchBreed = (params = {}) => fetch({
-  url: `${base_url}/breeds?${queryString.stringify(params)}`
-})
+export const fetchBreed = (params = {}) => fetch(`${base_url}/breeds?${queryString.stringify(params)}`)
 
-export const fetchCategory = (params = {}) => fetch({
-  url: `${base_url}/categories?${queryString.stringify(params)}`
-})
+export const fetchCategory = (params = {}) => fetch(`${base_url}/categories?${queryString.stringify(params)}`)
